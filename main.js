7e7c0c6ec38b69b40d7c562e3bf24fb8031ad947
kamekame7 <31509007+kamekame7@users.noreply.github.com>
2020-03-25 15:28:57 +0900

    a
    a

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+var h = $(window).height();
+
+$('.hidden').css('display','none');
+$('.pleasescroll').css('display','none');
+$('#showloading').height(h).css('display','block');
+
+});
+$('head').append(
+'<style>.hidden{display:none;}'
+);
+$(window).on("load", function() {
+
+$('#showloading').css('display','none');
+$('.hidden').delay(400).fadeIn(1000);
+$('.pleasescroll').delay(1500).fadeIn("slow");
+
+});
+$(function(){
+
+$(window).scroll(function (){
+
+  $('.scroll').each(function(){
+    var elemPos = $(this).offset().top;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll > elemPos - windowHeight + 300){
+      $(this).addClass('scrollin');
+    }
+  });
+});
+});
